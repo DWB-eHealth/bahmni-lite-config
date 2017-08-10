12b6851056674714f78332818e064336ed0862e3
sudhamshk <sudhamsh543@gmail.com>
2017-08-10 11:00:48 +0530

    Sudhamsh | adding bahmni-lite configurations

--- openmrs/apps/clinical/formConditions.js
+++ openmrs/apps/clinical/formConditions.js
@@ -12,6 +12,44 @@ Bahmni.ConceptSet.FormConditions.rules = {
             }
         }
     },
+    'Place of the visit': function (formName, formFieldValues) {
+        var conditions = {enable: [], disable: []};
+	var otherReasonLine = "Other";
+        var conditionConcept = formFieldValues['Place of the visit'];
+        if (conditionConcept == 'Other') {
+            conditions.enable.push("If other, specify:")
+        } else {
+            conditions.disable.push("If other, specify:")
+        }
+	return conditions;
+    },
+    'Change in diagnosis': function (formName, formFieldValues) {
+        var conditions = {
+            show: [],
+            hide: []
+        };
+        
+        var en4Drugs = "Diagnosis block";
+        var result = formFieldValues['Change in diagnosis'];
+        if (result == "1") {
+            conditions.show.push( en4Drugs);
+        } else {
+            conditions.hide.push( en4Drugs);
+        }
+        return conditions;
+    },
+
+    'Referral Out List': function (formName, formFieldValues) {
+        var conceptToEnable = "Other specialist";
+        var conditions = {enable: [], disable: []};
+        var Referral = formFieldValues['Referral Out List'];
+        if (Referral && (Referral == "Other" || Referral.value == "Other")) {
+            conditions.enable.push(conceptToEnable)
+        } else {
+            conditions.disable.push(conceptToEnable)
+        }
+        return conditions;
+    },
     'Systolic Data' : function (formName, formFieldValues) {
         var systolic = formFieldValues['Systolic'];
         var diastolic = formFieldValues['Diastolic'];
